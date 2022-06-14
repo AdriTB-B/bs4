0c4eea6657e2095ca4a64e9ede26d5f60fd8fa38
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-06-14 10:41:19 +0200

    Correcion

--- src/main/java/com/adri/bs4/Controlador.java
+++ src/main/java/com/adri/bs4/Controlador.java
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @ConfigurationProperties
 @RestController
 public class Controlador {
+    @Value("${VAR1}") String var1;
+    @Value("${My.VAR2}")  int var2;
+    @Value("${VAR3:var3 no tiene valor}") String var3;
 
     @GetMapping("/valores/")
-    public String getValue(
-            @Value("${VAR1}") String var1,
-            @Value("${My.VAR2}")  int var2
-    ){
+    public String getValue(){
         return "valor de var1 es: "+var1+ ", valor de my.var2 es: "+var2;
     }
 
     @GetMapping("/var3/")
-    public String getVar3(@Value("${VAR3:var3 no tiene valor}") String var3){
+    public String getVar3(){
         return  "valor de var3 es: "+var3;
     }
 
